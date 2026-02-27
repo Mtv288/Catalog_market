7e9e22fa8d0eb0c2f426d208b8e9d761cfa44fd3
Mtv288 <meremkulovtv@gmail.com>
2026-02-27 22:02:20 +0300

    1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-sfsddgdfg
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def main():
+    return {"hello"}
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
