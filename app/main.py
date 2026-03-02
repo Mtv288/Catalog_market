bb9814f2dd8fcfaafdee024d1341ba7e1a90a9d0
Mtv288 <meremkulovtv@gmail.com>
2026-03-02 18:16:30 +0300

    html great base

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from routers import main
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(main.router)
 
 
